e88de28086a452704b5215f050ea452b06c74216
huy <huy@huyng.com>
2013-04-26 02:27:11 -0700

    add tf idf

--- tfidf/tfidf.py
+++ tfidf/tfidf.py
@@ -1,9 +1,15 @@
+"""
+Term Frequency vs Inverse Document Frequency (tf-idf)
+is a common measure used for ranking how likely a
+document matches a search term.
+"""
+
 import numpy as np
 
 def tfidf_weights(term, documents):
     """
     Calculate the term-frequency inverse-document-frequency
-    for a given term in a set of documents
+    for a given term in a set of documents.
 
     :arg term:          A term
     :arg documents:     A list of documens composed of terms
@@ -13,8 +19,12 @@ def tfidf_weights(term, documents):
     >>> print tfidf_weights(term, docs)
 
     """
-    tf  = [d.count(term) for d in documents]  # term frequency in each documents
+
+    # term frequency in each documents
+    tf = [d.count(term)/float(len(d)) for d in documents]  
+
+    # document cardinality
+    docs_count = len(documents) 
     docs_containing_term = sum(term in d for d in documents) or 1
-    docs_count = len(documents)  # document cardinality
     idf = np.log(float(docs_count) / float(docs_containing_term))
     return np.array(tf) * idf

137a7d088186fe0700fdb91530df469f7cd29ab2
huy <huy@huyng.com>
2014-03-09 22:39:44 -0700

    working vp tree implementation

--- tSNE/README.md
+++ tSNE/README.md
@@ -1,8 +1,15 @@
 tSNE is a technique to project high dimensional data onto 2d or 3d for the purpose of data visualization.
 
-http://jmlr.org/papers/volume9/vandermaaten08a/vandermaaten08a.pdf
 
 Todo:
 - find quadtree implementation
+    - find way to compute force on point
+    - figure out how traverse tree to figure out where a "summary" node can be used to compute repulsion force
 - find vp tree implementation
-- figure out how traverse tree to figure out where a "summary" node can be used to compute repulsion force
+
+# References
+
+- [tSNE](http://jmlr.org/papers/volume9/vandermaaten08a/vandermaaten08a.pdf)
+- [vp-tree](http://aidblab.cse.iitm.ac.in/cs625/vptree.pdf)
+- [vp-tree](http://ceur-ws.org/Vol-256/submission_12.pdf)
+
--- tSNE/vptree.py
+++ tSNE/vptree.py
@@ -1,75 +1,170 @@
 from collections import namedtuple
+from collections import deque
 import random
 import numpy as np
-import deque
-from Queue import PriorityQueue
+import heapq
 
-### Data structures
+class NDPoint(object):
+    """
+    A point in n-dimensional space
+    """
 
-Point = namedtuple("Point", "x y")
+    def __init__(self, x, idx=None):
+        self.x = np.array(x)
+        self.idx = idx
+    def __repr__(self):
+        return "NDPoint(idx=%s, x=%s)" % (self.idx, self.x)
 
 class VPTree(object):
-    def __init__(self, points, dist_fn=l2):
+    """
+    An efficient data structure to perform nearest-neighbor
+    search. 
+    """
+
+    def __init__(self, points, dist_fn=None):
         self.left = None
         self.right = None
-        self.radius = None
+        self.mu = None
+        self.dist_fn = dist_fn if dist_fn is not None else l2
+
+        # choose a better vantage point selection process
         self.vp = points.pop(random.randrange(len(points)))
-        self.points = points
-        if len(points) <= 1:
+
+        if len(points) < 1:
             return
 
-        # child with all points inside radius
-        left_points = []
+        # choose division boundary at median of distances
+        distances = [self.dist_fn(self.vp, p) for p in points]
+        self.mu = np.median(distances)
 
-        # child with all points outside radius    
-        right_points = []
-
-        # compute distances
-        distances = [dist_fn(self.vp, p) for p in points]
-
-        # choose divisino boundary at median of distances
-        self.radius = np.median(distances)
-
+        left_points = []  # all points inside mu radius
+        right_points = []  # all points outside mu radius
         for i, p in enumerate(points):
             d = distances[i]
-            if d >= self.radius:
+            if d >= self.mu:
                 right_points.append(p)
             else:
                 left_points.append(p)
 
         if len(left_points) > 0:
-            self.left = VPTree(points=left_points, dist_fn=dist_fn)
+            self.left = VPTree(points=left_points, dist_fn=self.dist_fn)
 
         if len(right_points) > 0:
-            self.right = VPTree(points=right_points, dist_fn=dist_fn)
+            self.right = VPTree(points=right_points, dist_fn=self.dist_fn)
+
+    def is_leaf(self):
+        return (self.left is None) and (self.right is None)
+
+class PriorityQueue(object):
+    def __init__(self, size=None):
+        self.queue = []
+        self.size = size
+
+    def push(self, priority, item):
+        self.queue.append((priority, item))
+        self.queue.sort()
+        if self.size is not None and len(self.queue) > self.size:
+            self.queue.pop()
 
 
 ### Distance functions
-
 def l2(p1, p2):
-    return np.sqrt((p2.x-p1.x)**2 + (p2.y-p1.y)**2)
+    return np.sqrt(np.sum(np.power(p2.x - p1.x, 2)))
+
 
 ### Operations
+def get_nearest_neighbors(tree, q, k=1):
+    """
+    find k nearest neighbor(s) of q
 
-def NNSearch(tree, query, n, dist_fn=l2):
-    if tree is None:
-        return
-    neighbors = PriorityQueue(n)
+    :param tree:  vp-tree
+    :param q: a query point
+    :param k: number of nearest neighbors
+
+    """
+
+    # buffer for nearest neightbors
+    neighbors = PriorityQueue(k)
+
+    # list of nodes ot visit
     visit_stack = deque([tree])
+
+    # distance of n-nearest neighbors so far
     tau = np.inf
+
     while len(visit_stack) > 0:
-        node = visit_stack.pop()
-        d = dist(query, node.vp)
+        node = visit_stack.popleft()
+        if node is None:
+            continue
+
+        d = tree.dist_fn(q, node.vp)
+        if d < tau:
+            neighbors.push(d, node.vp)
+            tau, _ = neighbors.queue[-1]
+
+        if node.is_leaf():
+            continue
+
+        if d < node.mu:
+            if d < node.mu + tau:
+                visit_stack.append(node.left)
+            if d >= node.mu - tau:
+                visit_stack.append(node.right)
+        else:
+            if d >= node.mu - tau:
+                visit_stack.append(node.right)
+            if d < node.mu + tau:
+                visit_stack.append(node.left)
+    return neighbors.queue
 
 
+def get_all_in_range(tree, q, tau):
+    """
+    find all points within a given radius of point q
 
+    :param tree: vp-tree
+    :param q: a query point
+    :param tau: the maximum distance from point q
+    """
+
+    # buffer for nearest neightbors
+    neighbors = []
+
+    # list of nodes ot visit
+    visit_stack = deque([tree])
+
+    while len(visit_stack) > 0:
+        node = visit_stack.popleft()
+        if node is None:
+            continue
+
+        d = tree.dist_fn(q, node.vp)
+        if d < tau:
+            neighbors.append((d, node.vp))
+
+        if node.is_leaf():
+            continue
+
+        if d < node.mu:
+            if d < node.mu + tau:
+                visit_stack.append(node.left)
+            if d >= node.mu - tau:
+                visit_stack.append(node.right)
+        else:
+            if d >= node.mu - tau:
+                visit_stack.append(node.right)
+            if d < node.mu + tau:
+                visit_stack.append(node.left)
+    return neighbors
 
 
 
 if __name__ == '__main__':
-    X = np.random.uniform(0, 1000, size=40000)
-    Y = np.random.uniform(0, 1000, size=40000)
-    points = [Point(x,y) for i, (x, y) in  enumerate(zip(X,Y))]
+    X = np.random.uniform(0, 100000, size=40000)
+    Y = np.random.uniform(0, 100000, size=40000)
+    points = [NDPoint(x,i) for i, x in  enumerate(zip(X,Y))]
     tree = VPTree(points)
-
-            
+    q = NDPoint([300,300])
+    neighbors = get_nearest_neighbors(tree, q, 5)
+    for n in neighbors:
+        print n

ab3f03da00a19e04aedc48021bddfc6882b910a0
huy <huy@huyng.com>
2015-03-20 04:33:30 -0700

    proper random state

--- rnn/datasets.py
+++ rnn/datasets.py
@@ -16,8 +16,8 @@ def sinewaves(timesteps, n):
     """
     rng = np.random.RandomState(888)
     t = np.arange(timesteps).astype(config.floatX)
-    frequency_weights = rng.random(n).astype(config.floatX)
-    amplitude_weights = rng.random(n).astype(config.floatX)
+    frequency_weights = rng.rand(n).astype(config.floatX)
+    amplitude_weights = rng.rand(n).astype(config.floatX)
     t_scaled = frequency_weights[:, np.newaxis] * t
     x_train = amplitude_weights[:, np.newaxis] * np.sin(t_scaled)
     y_train = np.roll(x_train, -1, axis=1)
